f13a353c02fbbc159a87d62efd489b70b56be143
PatrykRudnicki <patrudnicki@gmail.com>
2016-12-21 10:52:46 +0100

    Search form

--- src/main/scala/code/snippet/Form.scala
+++ src/main/scala/code/snippet/Form.scala
@@ -5,6 +5,8 @@ import net.liftweb.common.Full
 import net.liftweb.http._
 import net.liftweb.util.Helpers._
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
   * Created by patry on 20.11.2016.
   */
@@ -13,6 +15,10 @@ class Form {
   var lastName = ""
   var age = ""
   var sex = ""
+  var searchField = ""
+  var choice = ""
+  var searchedUsers: ArrayBuffer[User] = ArrayBuffer[User]()
+
   private val formRenderer = SHtml.idMemoize { renderer =>
     ".firstName" #> SHtml.text(firstName, firstName = _) &
     ".lastName" #> SHtml.text(lastName, lastName = _) &
@@ -21,7 +27,6 @@ class Form {
     ".save" #> SHtml.ajaxOnSubmit(() => {
       val user = User(firstName, lastName, age, sex)
       Users.users += user
-      S.notice(Users.users(0).firstName + " " + Users.users(0).lastName + " " + Users.users(0).age)
       renderer.setHtml() & usersRenderer.setHtml()
     })
   }
@@ -35,6 +40,36 @@ class Form {
     }
   }
 
+  private val usersSearch = SHtml.idMemoize { renderer =>
+    ".search" #> SHtml.text(searchField, searchField = _) &
+      ".save" #> SHtml.ajaxOnSubmit(() => {
+        searchedUsers = Users.users.filter(_.lastName == searchField)
+        renderer.setHtml() & searchRenderer.setHtml()
+      })
+  }
+
+  private val usersSearch2 = SHtml.idMemoize { renderer =>
+    ".choice" #> SHtml.select(Seq(("first", "firstName"), ("last", "lastName"), ("age", "age"), ("sex", "sex")), Full("firstName"), choice = _) &
+    ".search" #> SHtml.text(searchField, searchField = _) &
+    ".save" #> SHtml.ajaxOnSubmit(() => {
+      choice match{
+        case "firstName" => searchedUsers = Users.users.filter(_.firstName == searchField)
+        case "lastName" => searchedUsers = Users.users.filter(_.lastName == searchField)
+        case "age" => searchedUsers = Users.users.filter(_.age == searchField)
+        case "sex" => searchedUsers = Users.users.filter(_.sex == searchField)
+      }
+      renderer.setHtml() & searchRenderer.setHtml()
+    })
+  }
+
+  private val searchRenderer = SHtml.idMemoize { renderer =>
+    ".users" #> searchedUsers.map{ user =>
+      ".firstName *" #> user.firstName &
+      ".lastName *" #> user.lastName &
+      ".age *" #> user.age &
+      ".sex *" #> user.sex
+    }
+  }
 
   def render = {
     SHtml.makeFormsAjax andThen
@@ -44,5 +79,15 @@ class Form {
 
   def renderUsers = {
     "#users" #> usersRenderer
+  }
+
+  def search = {
+    SHtml.makeFormsAjax andThen
+    "#search" #> usersSearch
+  }
+
+  def search2 = {
+    SHtml.makeFormsAjax andThen
+    "#search2" #> usersSearch2
   }
 }
